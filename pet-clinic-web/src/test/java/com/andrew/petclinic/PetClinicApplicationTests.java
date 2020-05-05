9397f5547ff846ef48692631efb4354759631457
Andrew Ngo <andrewtruongngo@gmail.com>
2020-05-04 18:11:24 -0700

    using maven release with test v7

--- pet-clinic-web/src/test/java/com/andrew/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/com/andrew/petclinic/PetClinicApplicationTests.java
@@ -3,9 +3,11 @@ package com.andrew.petclinic;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = com.andrew.petclinic.PetClinicApplication.class)
 @SpringBootTest
 class PetClinicApplicationTests {
 
